f004abf1225186ceca9227c642706d88d0f3bad6
jli198 <jli198@stevens.edu>
2024-02-27 20:53:23 -0500

    vim examples

---
+++ vim-bash/code.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void say_hello(void)
+{
+	printf("Hello, World!!!\n");
+	printf("Hello, World!!!\n");
+	printf("Hello, World!!!\n");
+	printf("Hello, World!!!\n");
+	printf("Hello, World!!!\n");
+	printf("Hello, World!!!\n");
+}
+
+int main(void)
+{
+	say_hello();
+
+	return 0;
+
+}        
